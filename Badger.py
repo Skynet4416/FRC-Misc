3abaec94d85c83ce95290fa5bd8e5ab53a1f148c
BigBoyJeff <eyalb2000@gmail.com>
2019-03-03 12:51:17 +0200

    Code works
    Maybe

--- Badger.py
+++ Badger.py
@@ -1,33 +1,76 @@
 from PIL import Image, ImageDraw, ImageFont
-from random import randint
 
-#Font is Montserrat
+# Font is Montserrat
 
-TAG_PATH = "Name-tag-Skynet2.png"
-# FONT_PATH = r"C:\Users\Nir\Anaconda3\Lib\site-packages\matplotlib\mpl-data\fonts\ttf\DejaVuSansMono.ttf"
-FONT_PATH = r"C:\Users\Nir\Documents\Git Projects\FRC-Misc\Font\Montserrat-Light.ttf"
+TAG_PATH = "MINI_TEMPLATE.png"
+FONT_PATH = r".\Font\Montserrat-Light.ttf"
+FONT_PATH2 = r".\Font\Montserrat-Light.ttf"
+MIN_NAME_SIZE = 175  # 1400
+MIN_ROLE_SIZE = 75  # 650
+LOW_NAME_Y = 110  # 800
+LOW_ROLE_Y = 30  # 270
+LEFT_SIDE_X = 160  # 1240
+ROCKET_RIGHT_X = 57  # 440
+CORRECTION_X = 5  # 30
+MAX_NAME_FONT = 40  # 286
+MAX_ROLE_FONT = 20  # 150
+FONT_JUMPS = 1  # 3
+
 with open("Names.txt") as f:
     names = []
-    for name in f.readlines():
-        names.append(name.split(","))
+    for name in f.read().split("\n"):
+        names.append(name.split("--"))
 print(names)
 
-img = Image.open(TAG_PATH)
-center = [s // 2 for s in img.size]
-position = (1000, 100)
-print(img.size)
+original = Image.open(TAG_PATH)
+original_size = (original.size[0], LOW_NAME_Y)
+center = [s // 2 for s in original_size]
+side_center = (original_size[0] + LEFT_SIDE_X) // 2
+print(original.size)
+print(side_center)
 
-# size = randint(250, 450)
-font = ImageFont.truetype(FONT_PATH, size=286)
-# print(size)
+name_fonts = dict()
+role_fonts = dict()
+name_fonts[MAX_NAME_FONT] = ImageFont.truetype(FONT_PATH, size=MAX_NAME_FONT)
+role_fonts[MAX_ROLE_FONT] = ImageFont.truetype(FONT_PATH2, size=MAX_ROLE_FONT)
 
-for name in names:
+
+name_centery = (original_size[1] + LOW_ROLE_Y) // 2
+for i, name in enumerate(names):
+    i += 23
+    img = original.copy()
     draw = ImageDraw.Draw(img)
-    size = draw.textsize(name[0], font=font)
+    font_size = MAX_NAME_FONT
+    center[0] = (original_size[0] + ROCKET_RIGHT_X) // 2
+    size = [original_size[0]]
+    while size[0] > MIN_NAME_SIZE:
+        font = name_fonts.get(font_size, None)
+        if font is None:
+            font = ImageFont.truetype(FONT_PATH, size=font_size)
+            name_fonts[font_size] = font
+        size = draw.textsize(name[0], font=font)
+        font_size -= FONT_JUMPS
+
     pos = []
-    pos.append(center[0] - size[0] // 2 + 50)
-    pos.append(center[1] - size[1] // 2  + 55)
-    # pos = [c - (p // 2) for c, p in zip(center, size)]
-    draw.text(pos, name[0], "orange", font=font)
-    draw.text((600, 100), name[1], (36, 36, 51))  # , font=font)
-    img.save("{}.png".format(name[0]))
+    pos.append(center[0] - size[0] // 2)
+    pos.append(name_centery - size[1] // 2)
+    print(name[0], pos)
+    draw.text(pos, name[0], "white", font=font)
+
+    font_size = MAX_ROLE_FONT
+    size = [original_size[0]]
+    while size[0] > MIN_ROLE_SIZE:
+        font = role_fonts.get(font_size, None)
+        if font is None:
+            font = ImageFont.truetype(FONT_PATH2, size=font_size)
+            role_fonts[font_size] = font
+        size = draw.textsize(name[1], font=font)
+        font_size -= FONT_JUMPS
+    pos = []
+    pos.append(side_center - size[0] // 2 - CORRECTION_X)
+    pos.append(LOW_ROLE_Y // 2 - size[1] // 2)
+    print(name[1], pos)
+    draw.text(pos, name[1], (36, 36, 51), font=font)
+    img.save("Tags\\{}-{}.png".format(i, name[0].strip(".")))
+
+print("Done")
